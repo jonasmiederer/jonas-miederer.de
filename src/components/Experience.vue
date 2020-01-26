4ba7d5490b905058d85327dfcd3e0f6a32c81e39
Jonas Miederer <jonasmied@gmail.com>
2020-01-26 21:50:31 +0100

    added lecturer experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -13,7 +13,7 @@
     <div class="col-12 col-lg-9 experience-right">
 
       <div class="steps-wrapper" v-for="(step, index) in steps">
-        <div :id="step.id">
+        <div :id="step.id" :class="step.yearEnd? '': 'current'">
           <div class="step-header">
           <span>
             <svg class="accent" focusable="false" viewBox="0 0 48 48" aria-hidden="true">
@@ -23,7 +23,7 @@
           </span>
             <h3 class="step-title">{{step.title}}</h3>
           </div>
-          <div class="row step-detail" :class="{'last-entry': index === steps.length - 1}">
+          <div class="row step-detail">
             <p v-html="step.details"></p>
           </div>
         </div>
@@ -34,61 +34,72 @@
 
 <script>
 
-  export default {
-    name: 'Experience',
+    export default {
+        name: 'Experience',
 
 
-    data() {
-      return {
-        steps: [
-          {
-            id: 'internship-11f',
-            yearStart: 2014,
-            yearEnd: 2015,
-            company: {
-              name: "11Freunde",
-              website: "https://www.11freunde.de/"
-            },
-            title: 'Practical semester / internship - Web Development',
-            details: "As part of my bachelor's program, I worked as an intern for 6 months at <a href=\"https://www.11freunde.de/\" target=\"_blank\">11Freunde Verlag</a> in Berlin. My scope of responsibility included developing new web applications (front- and backend), maintaining existing applications and databases as well as various IT-related tasks",
-          },
-          {
-            id: 'workingstudent-11f',
-            yearStart: 2015,
-            yearEnd: 2018,
-            company: {
-              name: "11Freunde",
-              website: "https://www.11freunde.de/"
-            },
-            title: 'Working student - Full Stack Development',
-            details: 'I very much liked the kind of work and freedom I had during my internship at 11Freunde. Therefore I was genuinely happy about the offer to continue as a working student being able to work completely remotely. Since I already gained some experience in Android development during my studies, I also assumed responsibility for maintenance of the <a href="https://play.google.com/store/apps/details?id=de.android.elffreunde" target="_blank">11Freunde App</a>  with its more than 100.000 accumulated downloads.',
-          },
-          {
-            id: 'master',
-            yearStart: 2017,
-            yearEnd: 2017,
-            company: {
-              name: "Daimler AG",
-              website: "https://www.daimler.com/"
-            },
-            title: "Master's candidate - Software Development Artificial Intelligence",
-            details: "After writing my bachelor's thesis at the university, I decided to approach my master's thesis in collaboration with industry. I started my work at the <a href='https://www.daimler.com/' target='_blank'>Daimler AG</a> as member of the #teamPactris, which again belongs to the <a href='https://www.daimler.com/company/strategy/digitallife/' target='_blank'>DigitalLife@Daimler</a> department.",
-          },
-          {
-            id: 'job-daimler',
-            yearStart: 2018,
-            yearEnd: null,
-            company: {
-              name: "Daimler AG",
-              website: "https://www.daimler.com/"
-            },
-            title: 'Software Developer Artificial Intelligence',
-            details: "With all the theoretical and practical experience I gained in the last years, I was perfectly prepared for my first fulltime job. And because I loved the work during my master's thesis, I am happy to still be part of this amazing team and to contribute my knowledge and expertise about AI and ML",
-          },
-        ],
-      }
+        data() {
+            return {
+                steps: [
+                    {
+                        id: 'internship-11f',
+                        yearStart: 2014,
+                        yearEnd: 2015,
+                        company: {
+                            name: "11Freunde",
+                            website: "https://www.11freunde.de/"
+                        },
+                        title: 'Practical semester / internship - Web Development',
+                        details: "As part of my bachelor's program, I worked as an intern for 6 months at <a href=\"https://www.11freunde.de/\" target=\"_blank\">11Freunde Verlag</a> in Berlin. My scope of responsibility included developing new web applications (front- and backend), maintaining existing applications and databases as well as various IT-related tasks",
+                    },
+                    {
+                        id: 'workingstudent-11f',
+                        yearStart: 2015,
+                        yearEnd: 2018,
+                        company: {
+                            name: "11Freunde",
+                            website: "https://www.11freunde.de/"
+                        },
+                        title: 'Working student - Full Stack Development',
+                        details: 'I very much liked the kind of work and freedom I had during my internship at 11Freunde. Therefore I was genuinely happy about the offer to continue as a working student being able to work completely remotely. Since I already gained some experience in Android development during my studies, I also assumed responsibility for maintenance of the <a href="https://play.google.com/store/apps/details?id=de.android.elffreunde" target="_blank">11Freunde App</a>  with its more than 100.000 accumulated downloads.',
+                    },
+                    {
+                        id: 'master',
+                        yearStart: 2017,
+                        yearEnd: 2017,
+                        company: {
+                            name: "Daimler AG",
+                            website: "https://www.daimler.com/"
+                        },
+                        title: "Master's candidate - Software Development Artificial Intelligence",
+                        details: "After writing my bachelor's thesis at the university, I decided to approach my master's thesis in collaboration with industry. I started my work at the <a href='https://www.daimler.com/' target='_blank'>Daimler AG</a> as member of the #teamPactris, which again belongs to the <a href='https://www.daimler.com/company/strategy/digitallife/' target='_blank'>DigitalLife@Daimler</a> department.",
+                    },
+                    {
+                        id: 'job-daimler',
+                        yearStart: 2018,
+                        yearEnd: null,
+                        company: {
+                            name: "Daimler AG",
+                            website: "https://www.daimler.com/"
+                        },
+                        title: 'Software Developer Artificial Intelligence',
+                        details: "With all the theoretical and practical experience I gained in the last years, I was perfectly prepared for my first fulltime job. And because I loved the work during my master's thesis, I am happy to still be part of this amazing team and to contribute my knowledge and expertise about AI and ML",
+                    },
+                    {
+                        id: 'dhbw',
+                        yearStart: 2019,
+                        yearEnd: null,
+                        company: {
+                            name: "DHBW Stuttgart",
+                            website: "https://www.dhbw-stuttgart.de/home/"
+                        },
+                        title: 'Lecturer at DHBW Stuttgart for Computer Science',
+                        details: "Besides my primary job I started a position as Lecturer at <a href='https://www.dhbw-stuttgart.de/home/'>DHBW (Cooperative State University) Stuttgart</a>, Campus Horb. I'm teaching computer science and programming to industrial engineering students, 1st and 2nd semester. I love to share my knowledge and passion about coding with the students and it also gives me a lot in return.",
+                    },
+                ],
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
@@ -110,7 +121,7 @@
   }
 
 
-  .step-detail.last-entry {
+  .current .step-detail {
     border-left: 1px dashed gray;
   }
 
