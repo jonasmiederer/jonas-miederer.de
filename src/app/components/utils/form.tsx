be00dfa18b7417eca78c1bbfc24cd0d457a53692
Jonas Miederer <hello@jonas-miederer.de>
2025-08-03 00:59:30 +0200

    fixed endpoint

--- src/app/components/utils/form.tsx
+++ src/app/components/utils/form.tsx
@@ -5,7 +5,7 @@ import { Input, Textarea } from "@heroui/input";
 import { useEffect, useState } from "react";
 import { addToast } from "@heroui/toast";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'error' ;
+const API_URL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL + 'contact' : 'error';
 
 export default function Form() {
 
